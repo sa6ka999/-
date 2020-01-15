0f4b3b42d779fb15d518f8fc511223c31cb367e6
sa6ka999 <55951370+sa6ka999@users.noreply.github.com>
2020-01-15 13:25:41 +0300

    Update TestAuthor.java

--- fifth/TestAuthor.java
+++ fifth/TestAuthor.java
@@ -3,9 +3,7 @@ package fifth;
 public class TestAuthor {
     public static void main(String[] args) {
         Author a1 = new Author("Александр", "krasnenkov888@gmail.com",'M');
-        System.out.println(a1);
-        a1.setEmail("krasnenkov888@gmail.com");
-        System.out.println(a1);
+        System.out.println(a1.toString());
     }
 }
 
